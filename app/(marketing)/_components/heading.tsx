a78e573d9fd11d99c3bf242b6f7da14072ab787c
Nhan Le <tiennhan223@gmail.com>
2024-03-01 19:13:11 -0700

    adding login/sign out feature, also make the button reflect with login or sign out status

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -4,8 +4,14 @@ a Server and Client Component modules. */
 "use client";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useContext } from "react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/ui/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () =>{
+    const {isAuthenticated, isLoading} = useConvexAuth();
     return(
         /*max-w-3xl: limite how width this heading can go*/
         <div className="max-w-3xl space-y-4">
@@ -18,10 +24,28 @@ export const Heading = () =>{
                 Jotion is the connected workspace where <br />
                 better, faster work happens.
             </h3>
-            <Button>
-                Enter Jotion
-                <ArrowRight className="h-5 w-5 ml-3"/>
+            {isLoading && (
+                <div className="flex w-full items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+            <Button asChild>
+                <Link href="/documents">
+                    Enter Jotion
+                    <ArrowRight className="h-5 w-5 ml-3"/>
+                </Link>
             </Button>
+            )}
+
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Jotion fee
+                        <ArrowRight className="h-5 w-5 ml-3"/>
+                    </Button>                
+                </SignInButton>
+            )}
         </div>
     )
 }
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,7 +5,10 @@ import { ModeToggle } from "@/components/mode-toggle-button";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo"
 import { useConvexAuth } from "convex/react";
-
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { Link } from "lucide-react";
 
 export const Navbar = () => {
     const scrolled = useScrollTop();
@@ -18,6 +21,34 @@ export const Navbar = () => {
             <Logo />
             <div className="md:ml-auto md:justify-end
             justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isLoading &&  !isAuthenticated && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Login
+                            </Button>
+                        </SignInButton>
+
+                        <SignInButton mode="modal">
+                            <Button  size="sm">
+                                Join Jotion free
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Jotion
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/"/>
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
